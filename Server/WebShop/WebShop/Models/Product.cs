217b03e6d4c88a71fc76086a3b2b8a8baf039de4
DusanInfinity <dusanantic099@gmail.com>
2022-01-09 16:09:49 +0100

    Dodati novi atributi klase Product

--- Server/WebShop/WebShop/Models/Product.cs
+++ Server/WebShop/WebShop/Models/Product.cs
@@ -6,7 +6,9 @@ namespace WebShop.Models
     {
         public string Name { get; set; }
         public int Price { get; set; }
+        public int Quantity { get; set; }
         public string Description { get; set; }
+        public string Image { get; set; }
         public List<string> Tags { get; set; }
         public List<ProductComment> Comments { get; set; }
     }
--- Server/WebShop/WebShop/Models/ProductComment.cs
+++ Server/WebShop/WebShop/Models/ProductComment.cs
@@ -5,7 +5,8 @@ namespace WebShop.Models
     public class ProductComment
     {
         public Product Product { get; set; }
-        public string Author { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
     }
